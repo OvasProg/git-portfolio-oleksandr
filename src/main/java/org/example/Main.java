387e99d568a2a649683d9e9695d0de87240d8c75
OvasProg <alex.vasyliev.prog@gmail.com>
2026-03-30 18:02:34 +0300

    Update welcome message to indicate advanced features

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Calculator!");
+        System.out.println("Welcome to the Advanced Java Calculator!");
 
         InputParser parser = new InputParser();
         Calculator calculator = new BasicCalculator();
