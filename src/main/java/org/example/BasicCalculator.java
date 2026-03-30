9ba57eaeb4b4eff10992f8efb42096df7171bec7
OvasProg <alex.vasyliev.prog@gmail.com>
2026-03-30 17:59:08 +0300

    Add multiplication and division logic with zero-check

--- src/main/java/org/example/BasicCalculator.java
+++ src/main/java/org/example/BasicCalculator.java
@@ -8,6 +8,13 @@ public class BasicCalculator implements Calculator {
                 return a + b;
             case SUBTRACT:
                 return a - b;
+            case MULTIPLY:
+                return a * b;
+            case DIVIDE:
+                if (b == 0) {
+                    throw new ArithmeticException("Cannot divide by zero.");
+                }
+                return a / b;
             default:
                 throw new UnsupportedOperationException("Operation not supported yet.");
         }
